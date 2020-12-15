50d384ed8641c70a47e11ed706fbe1a9b43fc402
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 23:32:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
+  attr_reader :sender, :receiver, :status
   # your code here
+  def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
 end
