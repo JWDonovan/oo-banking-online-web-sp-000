4447420f42a417cda5ba8677ecdf5809bbdac283
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 23:40:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,4 +22,12 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    if valid? && @receiver.balance > @amount && @status === "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
 end
